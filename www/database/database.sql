94f7c2316716dbac4a5835fe8cf07ac50e23c0b0
gogetashenron <150448257+gogetashenron@users.noreply.github.com>
2023-12-01 19:11:14 +0100

    Update database.sql

--- www/database/database.sql
+++ www/database/database.sql
@@ -2,7 +2,8 @@
 SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `utente`;
 DROP TABLE IF EXISTS `disegni`;
-DROP TABLE IF EXISTS `metodopagamento`;
+DROP TABLE IF EXISTS `carta`;
+DROP TABLE IF EXISTS `acquisti`;
 SET FOREIGN_KEY_CHECKS = 1;
 
 --
@@ -31,47 +32,17 @@ INSERT INTO `disegni` (`nome`, `disegno`, `descrizione`, `autore`) VALUES
 ('sorriso inquietante', '..\images\immagini\9.png', 'sorriso inquietante', 'osama'),
 ('sguardo accattivante', '..\images\immagini\10.png', 'sguardo accattivante', 'rupi');
 
---
--- Struttura della tabella `metodopagamento`
---
-CREATE TABLE `metodopagamento` (
-  `id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  `metodopagamento` varchar(30),
-  `nomedisegno` varchar(30),
-  `nomeordinante` varchar(40),
-  `nomebeneficiario` varchar(40),
-  `numerocarta` varchar(16),
-  `datascadenza` date,
-  `cvc` varchar(3),
-  FOREIGN KEY (nomedisegno) REFERENCES disegni(nome) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
---
--- Dump dei dati per la tabella `metodopagamento`
---
-INSERT INTO `metodopagamento` (`metodopagamento`, `nomedisegno`, `nomeordinante`, `nomebeneficiario`, `numerocarta`, `datascadenza`, `cvc`) VALUES
-('carta', 'felice distorto', 'Marco', 'Francesco', '1513766270350508', '2025-05-12', '438'),
-('carta', 'sorriso con gli occhiali', 'Luca', 'Chiara', '6029916024957293', '2025-09-03', '264'),
-('carta', 'non soddisfatto', 'Stefano', 'Sofia', '8995918624320528', '2025-11-21', '427'),
-('carta', 'wow', 'Giovanni', 'Giorgia', '9350791418136922', '2026-03-07', '527'),
-('carta', 'non impressionato', 'Laura', 'Elena', '0954444877704974', '2026-07-18', '665'),
-('carta', 'una roccia', 'Paolo', 'Martina', '0967356306490846', '2023-04-15', '117'),
-('carta', 'sguardo fisso', 'Davide', 'Luca', '5796213327187679', '2023-08-02', '039'),
-('carta', 'cane', 'Francesco', 'Marco', '6089308417790668', '2023-11-22', '016'),
-('carta', 'sorriso inquietante', 'Mauro', 'Alessandro', '8213238592311246', '2024-03-10', '646'),
-('carta', 'sguardo accattivante', 'Franco', 'Massimiliano', '3058022108081899', '2024-06-28', '601');
 
 --
 -- Struttura della tabella `utente`
 --
-CREATE TABLE `utente` (
-  `id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+CREATE TABLE `utente`(
   `username` varchar(30) NOT NULL,
   `nome` varchar(30),
   `cognome` varchar(30),
   `password` varchar(20),
   `email` varchar(30),
-  FOREIGN KEY (id) REFERENCES metodopagamento(id) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY(username)
 );
 
 --
@@ -88,3 +59,54 @@ INSERT INTO `utente` (`username`, `nome`, `cognome`, `password`, `email`) VALUES
 ('QuantumJazz', 'Luca', 'Conti', 'Dd6lb4f38xVuEBX', 'QuantumJazz@gmail.com'),
 ('SolarFlareQuest', 'Francesco', 'Lombardi', 'BkkM3h58wrpfZ2Y', 'SolarFlareQuest@gmail.com'),
 ('ZenithWhisperer', 'Mauro', 'Ferrara', 'lEko1RWez1sno6U', 'ZenithWhisperer@gmail.com');
+
+--
+-- Struttura della tabella `carta`
+--
+CREATE TABLE `carta` (
+  `username` varchar(30),
+  `numerocarta` varchar(16),
+  `datascadenza` date,
+  `cvc` varchar(3),
+  PRIMARY KEY(numerocarta),
+  FOREIGN KEY (username) REFERENCES utente(username) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+--
+-- Dump dei dati per la tabella `carta`
+--
+INSERT INTO `carta` (`username`,`numerocarta`, `datascadenza`, `cvc`) VALUES
+('BlueSkyWalker', '1513766270350508', '2025-05-12', '438'),
+('QuantumJazz', '6029916024957293', '2025-09-03', '264'),
+('PixelPioneer', '8995918624320528', '2025-11-21', '427'),
+('LunaHarmony', '9350791418136922', '2026-03-07', '527'),
+('NebulaDreamer', '0954444877704974', '2026-07-18', '665'),
+('CipherNomad', '0967356306490846', '2023-04-15', '117'),
+('MidnightSerenade', '5796213327187679', '2023-08-02', '039'),
+('SolarFlareQuest', '6089308417790668', '2023-11-22', '016'),
+('ZenithWhisperer', '8213238592311246', '2024-03-10', '646'),
+('EchoRhythm', '3058022108081899', '2024-06-28', '601');
+
+CREATE TABLE `acquisti` (
+  `IDAcquisto` int AUTO_INCREMENT,
+  `username` varchar(30),
+  `disegno` varchar(40),
+  `dataAcquisto`date,
+  `prezzo` int,
+  PRIMARY KEY(IDAcquisto),
+  FOREIGN KEY(username) REFERENCES utente(username) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY(disegno) REFERENCES disegni(nome) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO acquisti (username, disegno, dataAcquisto, prezzo) VALUES
+  ('BlueSkyWalker', 'felice distorto', '2023-05-15', 20),
+  ('QuantumJazz', 'sorriso con gli occhiali', '2023-06-20', 15),
+  ('PixelPioneer', 'non soddisfatto', '2023-07-10', 30),
+  ('LunaHarmony', 'wow', '2023-08-05', 25),
+  ('NebulaDreamer', 'non impressionato', '2023-09-12', 18),
+  ('CipherNomad', 'una roccia', '2023-10-25', 12),
+  ('MidnightSerenade', 'sguardo fisso', '2023-11-30', 22),
+  ('SolarFlareQuest', 'cane', '2023-12-15', 35),
+  ('ZenithWhisperer', 'sorriso inquietante', '2024-01-02', 28),
+  ('EchoRhythm', 'sguardo accattivante', '2024-02-18', 19);
+
