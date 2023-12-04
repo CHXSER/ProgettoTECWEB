65d9285c959f4c44165cb268b1a8a06261887fe2
gogetashenron <150448257+gogetashenron@users.noreply.github.com>
2023-12-04 20:34:20 +0100

    Update database.sql

--- www/database/database.sql
+++ www/database/database.sql
@@ -113,4 +113,4 @@ INSERT INTO acquisti (username, disegno, dataAcquisto, prezzo) VALUES
   ('QuantumJazz', 'non soddisfatto', '2024-04-02', 30),
   ('PixelPioneer', 'wow', '2024-05-10', 22),
   ('LunaHarmony', 'felice distorto', '2024-06-18', 18),
-  ('MidnightSerenade', 'sorriso con gli occhiali', '2024-07-25', 35);
+  ('MidnightSerenade', 'sguardo accattivante', '2024-07-25', 35);
