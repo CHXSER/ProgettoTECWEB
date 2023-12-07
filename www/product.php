ab65302f6e82a2f166680910d827e3df056d4fb6
CHXSER <elmoutaren@gmail.com>
2023-12-07 16:58:06 +0100

    Fixed product.php

--- www/product.php
+++ www/product.php
@@ -11,7 +11,7 @@
         $nome = $_GET["nome"];
         $row = get_drawing($nome);
         $DOM = str_replace("<!-- Immagine -->",  "./images/immagini/" . $row[0]["disegno"], $DOM);
-        $DOM = str_replace("<h2 class=\"heading\" id=\"nome\"></h2>", "<h2 class=\"heading\" id=\"nome\">" . $row[0]["nome"] . "</h2>", $DOM);
+        $DOM = str_replace("<!-- Nome -->", $row[0]["nome"], $DOM);
         $DOM = str_replace("<!-- Prezzo -->", $row[0]["prezzo"] . " €", $DOM);
         //$DOM = str_replace("<p></p>", "<p>" . $row[0]["descrizione"] . "</p>", $DOM);
         $DOM = str_replace("<p class=\"author\"></p>", "<p class=\"author\">Autore: <span>" . $row[0]["autore"] . "</span></p>", $DOM);
