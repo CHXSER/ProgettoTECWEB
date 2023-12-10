043588e3831773a8c73d854115abea69fcccd24d
Osama <oslaman@proton.me>
2023-12-10 16:35:25 +0100

    Delete unused font files and update button labels

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -83,3 +83,19 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].classList.add("hide-tab");
+  }
+
+  tablinks = document.getElementsByClassName("tab-links");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  document.getElementById(tabName).classList.remove("hide-tab");
+  evt.currentTarget.classList.add("active");
+}
