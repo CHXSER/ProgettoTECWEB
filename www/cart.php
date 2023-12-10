fef2e0254f27dedc45a95fa2dada5c9bc0a0655d
CHXSER <elmoutaren@gmail.com>
2023-12-10 16:21:59 +0100

    Mail sender

--- www/cart.php
+++ www/cart.php
@@ -5,10 +5,11 @@
     $description = "Visualizza elementi nel carello e procedi al pagamento";
     $keywords = "carello, cart";
 
+    session_start();
+
+
 
     include "php/template/header.php";
-
-    session_start();
 
     $DOM = file_get_contents("html/cart.html");
     echo ($DOM);
--- www/contact.php
+++ www/contact.php
@@ -4,6 +4,14 @@
     $description = "Scopri le origini di BOHEMY e tutti i suoi segreti";
     $keywords = "BOHEMY, origini, segreti, faq";
 
+    if(isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST["messaggio"])) {
+
+        $to = $_POST["email"];
+        $subject = "Abbiamo ricevuto la tua domanda";
+        $message = "Ciao " . $_POST["nome"] . ", La preghiamo di aspettare la risposta";
+        $mailSuccess = mail($to, $subject, $message);
+    }
+
     include "php/template/header.php";
     $DOM = file_get_contents("html/contact.html");
     echo ($DOM);
