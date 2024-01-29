674b4c94ea974d684fe7f4a532666ab0b0791f8d
oslaman <oslaman@proton.me>
2024-01-29 15:42:28 +0100

    compressione css e aggiornata la relazione

--- www/scripts/validate.js
+++ www/scripts/validate.js
@@ -1,5 +1,4 @@
 function validateUsername(username) {
-    // Check if the username is empty
     if (username.trim() === '' || !/^[a-zA-Z0-9]+$/.test(username) || username.length < 3 || username.length > 30) {
         return false;
     }
@@ -8,7 +7,6 @@ function validateUsername(username) {
 }
 
 function validateEmail(email) {
-    // Controlla se l'email è vuota
     if (email.trim() === '' || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
         return false;
     }
@@ -17,7 +15,6 @@ function validateEmail(email) {
 }
 
 function validatePassword(password) {
-    // Controlla se la password è vuota
     if (password.trim() === '' || password.length < 8 || password.length > 30) {
         return false;
     }
@@ -26,7 +23,6 @@ function validatePassword(password) {
 }
 
 function validateConfirmPassword(password, confirmPassword) {
-    // Controlla se la conferma della password è vuota
     if (confirmPassword.trim() === '' || password !== confirmPassword) {
         return false;
     }
@@ -35,17 +31,14 @@ function validateConfirmPassword(password, confirmPassword) {
 }
 
 function validateLoginForm() {
-    // Ottieni i valori dagli input del form
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
-    // Valida lo username
     if (username.trim() === '') {
         document.getElementById('form-error').innerHTML = 'Username non valido'
         return false;
     }
 
-    // Valida la password
     if (password.trim() === '') {
         document.getElementById('form-error').innerHTML = 'Password non valida'
         return false;
@@ -55,31 +48,26 @@ function validateLoginForm() {
 }
 
 function validateRegisterForm() {
-    // Ottieni i valori dagli input del form
     let username = document.getElementById('username').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
     let confirmPassword = document.getElementById('confirm-password').value;
 
-    // Valida lo username
     if (!validateUsername(username)) {
         document.getElementById('form-error').innerHTML = 'Username non valido';
         return false;
     }
 
-    // Valida l'indirizzo email
     if (!validateEmail(email)) {
         document.getElementById('form-error').innerHTML = 'Email non valida';
         return false;
     }
 
-    // Valida la password
     if (!validatePassword(password)) {
         document.getElementById('form-error').innerHTML = 'La password deve essere lunga almeno 8 caratteri e non più di 30'
         return false;
     }
 
-    // Valida la conferma della password
     if (!validateConfirmPassword(password, confirmPassword)) {
         document.getElementById('form-error').innerHTML = 'Le password non corrispondono'
         return false;
@@ -89,31 +77,26 @@ function validateRegisterForm() {
 }
 
 function validateAddProductForm() {
-    // Ottieni i valori dagli input del form
     let name = document.getElementById('nome').value;
     let price = document.getElementById('prezzo').value;
     let description = document.getElementById('descrizione').value;
     let image = document.getElementById('immagine').value;
 
-    // Valida il nome
     if (!validateName(name)) {
         document.getElementById('form-error').innerHTML = 'Nome non valido';
         return false;
     }
 
-    // Valida il prezzo
     if (!validatePrice(price)) {
         document.getElementById('form-error').innerHTML = 'Prezzo non valido';
         return false;
     }
 
-    // Valida la descrizione
     if (!validateDescription(description)) {
         document.getElementById('form-error').innerHTML = 'Descrizione non valida';
         return false;
     }
 
-    // Valida l'immagine
     if (!validateImage(image)) {
         document.getElementById('form-error').innerHTML = 'Immagine non valida';
         return false;
@@ -123,7 +106,6 @@ function validateAddProductForm() {
 }
 
 function validatePrice(price) {
-    // Controlla se il prezzo è vuoto
     if (price.trim() === '' || isNaN(price)) {
         return false;
     }
@@ -132,7 +114,6 @@ function validatePrice(price) {
 }
 
 function validateDescription(description) {
-    // Controlla se la descrizione è vuota
     if (description.trim() === '' || description.length < 3 || description.length > 30) {
         return false;
     }
@@ -141,7 +122,6 @@ function validateDescription(description) {
 }
 
 function validateImage(image) {
-    // Controlla se l'immagine è vuota
     if (image.trim() === '') {
         return false;
     }
@@ -150,10 +130,8 @@ function validateImage(image) {
 }
 
 function validateAddToCartForm() {
-    // Ottieni i valori dagli input del form
     let quantity = document.getElementById('quantity').value;
 
-    // Valida la quantità
     if (!validateQuantity(quantity)) {
         document.getElementById('form-error').innerHTML = 'Quantità non valida';
         return false;
@@ -163,12 +141,10 @@ function validateAddToCartForm() {
 }
 
 function validateQuantity(quantity) {
-    // Controllo se la quantità è vuota
     if (quantity.trim() === '') {
         return false;
     }
 
-    // Controllo se la quantità è un numero
     if (isNaN(quantity)) {
         return false;
     }
